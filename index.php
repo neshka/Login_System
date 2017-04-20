09d65d9c604dfad4a750584b7eb91b300986659c
neshka <neshka_mind@yahoo.com>
2017-04-21 00:53:10 +0100

    add login form

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Log in if you dare</title>
+</head>
+<body>
+<hr>
+<br>
+"It ain't what you don't know that gets you into trouble.
+<br>
+It's what you know for sure that just ain't so."
+<br><br>
+Mark Twain
+<br><br><br><hr>
+<form>
+    <br>
+    Login:<br><input type="text" name="login">
+    <br><br>
+    Password:<br><input type="password" name="password">
+    <br><br><br>
+    <input type="submit" value="Sign in">
+    <br><br><hr>
+</form>
+</body>
+</html>
